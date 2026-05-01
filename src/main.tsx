91e8b23a91d05154423f01029f44b2eaf312e52c
Simone Faricelli <16625698+SimoneFaricelli@users.noreply.github.com>
2026-05-01 17:02:23 +0200

    def_8

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// 🔥 FIX GITHUB PAGES ROUTING
+const redirect = new URLSearchParams(window.location.search).get("redirect");
+
+if (redirect) {
+  window.history.replaceState(null, "", redirect);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
