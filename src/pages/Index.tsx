751fbe9c94289c96c159c62211af30ee7ad19568
Simone Faricelli <16625698+SimoneFaricelli@users.noreply.github.com>
2026-05-01 12:55:19 +0200

    def_3

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+import  Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import AboutSection from "@/components/AboutSection";
 import ContactSection from "@/components/ContactSection";
