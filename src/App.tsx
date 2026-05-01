c8012bc0451e90bf454136558543d1b7c531ab8e
Simone Faricelli <16625698+SimoneFaricelli@users.noreply.github.com>
2026-05-01 16:37:51 +0200

    def_4

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@/tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useSystemTheme } from "@/hooks/useSystemTheme";
 import Index from "./pages/Index";
@@ -21,7 +20,8 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/" element={<TutorAI />} />
+          
+          <Route path="/products/tutor-ai" element={<TutorAI />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
           
